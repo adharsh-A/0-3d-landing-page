396c3299c46b08d5fb61fead5c9a5b5d9ae02b82
adharsh-A <103622525+adharsh-A@users.noreply.github.com>
2025-03-07 00:16:26 +0530

    after three

--- src/three.jsx
+++ src/three.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
-import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
-import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
-import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import Loader from "./components/Loader.jsx";
 
 const Animation = () => {
